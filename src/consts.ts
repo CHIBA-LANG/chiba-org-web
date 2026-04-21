3f08c923229917bd021ee59c6994bbc53d03ccc0
LemonHX <lemonhx@lemonhx.moe>
2026-04-21 21:38:23 +0200

    fix: render issue and SEO issue

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'CHIBA LANG';
+export const SITE_DESCRIPTION =
+	'Metal-level functional zero runtime infrastructure. Deterministic compilation with Automated Theorem Proving for formal memory verification.';
